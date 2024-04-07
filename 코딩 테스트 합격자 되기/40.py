5ad8d9d5bfe92c1b3d079e13c7fd2a487707dd9a
chanwon <clw8679@naver.com>
2024-04-07 18:07:34 +0900

    40. 다익스트라 알고리즘

---
+++ 코딩 테스트 합격자 되기/40.py
@@ -0,0 +1,26 @@
+import heapq
+
+def solution(graph, start):
+    distances = {node: float("inf") for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    paths = {start: [start]}
+    visited = set()
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+
+        for adjacent_node, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[adjacent_node]:
+                distances[adjacent_node] = distance
+                paths[adjacent_node] = paths[current_node] + [adjacent_node]
+                heapq.heappush(queue, [distance, adjacent_node])
+
+    sorted_paths = {node: paths[node] for node in sorted(paths)}
+
+    return [distances, sorted_paths]
