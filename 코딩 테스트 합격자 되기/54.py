05b0a57e0215c1e41c2418cf0be619a3abf3eae9
chanwon <clw8679@naver.com>
2024-04-21 22:43:17 +0900

    54. 계수 정렬 구현하기

---
+++ 코딩 테스트 합격자 되기/54.py
@@ -0,0 +1,11 @@
+def solution(s):
+    counts = [0] * 26
+
+    for c in s:
+        counts[ord(c) - ord("a")] += 1
+
+    sorted_str = ""
+    for i in range(26):
+        sorted_str += chr(i + ord("a")) * counts[i]
+
+    return sorted_str
