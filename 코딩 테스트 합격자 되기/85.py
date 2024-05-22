9060078e2e5db0b7fecc2e0c3e2c09033a3e77b0
chanwon <clw8679@naver.com>
2024-05-22 21:47:11 +0900

    85. 기지국 설치

---
+++ 코딩 테스트 합격자 되기/85.py
@@ -0,0 +1,13 @@
+def solution(n, stations, w):
+    answer = 0
+    location = 1
+    idx = 0
+
+    while location <= n:
+        if idx < len(stations) and location >= stations[idx] - w:
+            location = stations[idx] + w + 1
+            idx += 1
+        else:
+            location += 2 * w + 1
+            answer += 1
+    return answer
