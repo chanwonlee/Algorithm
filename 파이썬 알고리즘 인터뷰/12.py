6fb61e270039d081cbc95ad2ee60f3668b0fca63
chanwon <clw8679@naver.com>
2024-06-03 22:57:45 +0900

    12. 주식을 사고팔기 가장 좋은 시점

---
+++ 파이썬 알고리즘 인터뷰/12.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def maxProfit(prices):
+    profit = 0
+    min_price = sys.maxsize
+
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+
+    return profit
