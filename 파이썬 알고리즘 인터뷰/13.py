fdbca467c552608dcf4ae34c3ab98f01309b4b1a
chanwon <clw8679@naver.com>
2024-06-04 23:36:53 +0900

    13. 팰린드롬 연결 리스트

---
+++ 파이썬 알고리즘 인터뷰/13.py
@@ -0,0 +1,19 @@
+import collections
+
+
+def isPalindrome(head):
+    q = collections.deque()
+
+    if not head:
+        return True
+
+    node = head
+    while node is not None:
+        q.append(node.val)
+        node = node.next
+
+    while len(q) > 1:
+        if q.popleft() != q.pop():
+            return False
+
+    return True
