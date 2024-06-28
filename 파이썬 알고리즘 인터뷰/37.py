9f903f9d8fee26448afa7d8b371ebb3ea758fa20
chanwon <clw8679@naver.com>
2024-06-28 22:49:02 +0900

    37. 부분 집합

---
+++ 파이썬 알고리즘 인터뷰/37.py
@@ -0,0 +1,11 @@
+def subsets(nums):
+    result = []
+
+    def dfs(index, path):
+        result.append(path)
+
+        for i in range(index, len(nums)):
+            dfs(i + 1, path + [nums[i]])
+
+    dfs(0, [])
+    return result
