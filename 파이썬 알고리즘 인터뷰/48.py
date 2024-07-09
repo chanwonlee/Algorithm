9135d7a547e1b3d58c41edd5b7918868869a782a
chanwon <clw8679@naver.com>
2024-07-09 22:29:03 +0900

    48. 균형 이진 트리

---
+++ 파이썬 알고리즘 인터뷰/48.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def isBalanced(root):
+    def check(root):
+        if not root:
+            return 0
+
+        left = check(root.left)
+        right = check(root.right)
+
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
+
+    return check(root) != -1
