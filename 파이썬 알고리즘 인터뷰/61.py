cdc1adea2f94f5659aebbf84373336bd112f3d23
chanwon <clw8679@naver.com>
2024-07-24 12:55:17 +0900

    61. 가장 큰 수

---
+++ 파이썬 알고리즘 인터뷰/61.py
@@ -0,0 +1,15 @@
+def to_swap(n1, n2):
+    return str(n1) + str(n2) < str(n2) + str(n1)
+
+
+# 삽입 정렬 구현
+def largestNumber(nums):
+    i = 1
+    while i < len(nums):
+        j = i
+        while j > 0 and to_swap(nums[j - 1], nums[j]):
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            j -= 1
+        i += 1
+
+    return str(int(''.join(map(str, nums))))
