9ca2c5fa84142ce65bad6e2691b8f71de3fe3293
chanwon <clw8679@naver.com>
2024-08-13 13:39:11 +0900

    81. 주유소

---
+++ 파이썬 알고리즘 인터뷰/81.py
@@ -0,0 +1,13 @@
+def canCompleteCircuit(gas, cost):
+    if sum(gas) < sum(cost):
+        return -1
+
+    start, fuel = 0, 0
+    for i in range(len(gas)):
+        if gas[i] + fuel < cost[i]:
+            start = i + 1
+            fuel = 0
+        else:
+            fuel += gas[i] - cost[i]
+
+    return start
