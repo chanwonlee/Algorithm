93ffbf78c3611b071082a304274a5aa650515676
chanwon <clw8679@naver.com>
2024-10-08 15:15:42 +0900

    15989. 1, 2, 3 더하기 4

---
+++ 백준/15989.py
@@ -0,0 +1,11 @@
+d = [1] * 10001
+
+for i in range(2, 10001):
+    d[i] += d[i - 2]
+
+for i in range(3, 10001):
+    d[i] += d[i - 3]
+
+T = int(input())
+for _ in range(T):
+    print(d[int(input())])
