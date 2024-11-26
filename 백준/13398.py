69b77c4e1fe7842af5d19859702ca339ee6cf659
chanwon <clw8679@naver.com>
2024-11-26 23:19:29 +0900

    13398. 연속합 2

---
+++ 백준/13398.py
@@ -0,0 +1,9 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+dp = [[x for x in numbers] for _ in range(2)]
+
+for i in range(1, N):
+    dp[0][i] = max(dp[0][i - 1] + numbers[i], dp[0][i])
+    dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + numbers[i])
+
+print(max(max(dp[0]), max(dp[1])))
