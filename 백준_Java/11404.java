59901f8e8db811fb1c0162cc0d973d2d729ff28f
chanwon <clw8679@naver.com>
2026-02-02 21:23:52 +0900

    11404. 플로이드

---
+++ 백준_Java/11404.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                graph[i][j] = 10000000;
+                if (i == j) graph[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[start][end] = Math.min(graph[start][end], cost);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=n; j++) {
+                if (graph[i][j] == 10000000) System.out.print("0 ");
+                else System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
