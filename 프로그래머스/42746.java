c2e83b7cd3182f9209d7c45a1de6fccc9a1db7bb
chanwonlee <clw8679@naver.com>
2026-07-31 23:17:16 +0900

    42746. 가장 큰 수

---
+++ 프로그래머스/42746.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strs = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strs[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s);
+        }
+        
+        String result = sb.toString();
+        
+        if (result.charAt(0) == '0') {
+            return "0";
+        }
+        
+        return result;
+    }
+}
